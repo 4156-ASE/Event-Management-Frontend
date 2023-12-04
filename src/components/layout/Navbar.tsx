d639f3217b2b273f59943ed51eff7d7e3d3df101
Xzh0u <xzh0u.sxz@gmail.com>
2023-12-04 13:11:02 -0500

    feat(nav): routes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,17 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import { useContext } from 'react';
 import { AuthContext } from '../auth/AuthContextProvider';
 import { Nav, Dropdown, Button } from '@douyinfe/semi-ui';
 
-import { IconIntro, IconDescriptions, IconCalendar } from '@douyinfe/semi-icons-lab';
+import { IconIntro, IconCalendar, IconAvatar } from '@douyinfe/semi-icons-lab';
+import { IconCalendarStroked, IconUserStroked, IconHome } from '@douyinfe/semi-icons';
 import { APIs } from '../../utils/api';
 
 const Navbar = () => {
   const { auth, setAuth } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleSignOut = async () => {
     try {
@@ -31,18 +33,11 @@ const Navbar = () => {
       position="bottomRight"
       render={
         <Dropdown.Menu className="w-36">
-          <Dropdown.Item
-            onClick={() => {
-              navigate('/signup');
-            }}
-          >
-            SignUp
-          </Dropdown.Item>
           <Dropdown.Item onClick={handleSignOut}>Logout</Dropdown.Item>
         </Dropdown.Menu>
       }
     >
-      <Button>User</Button>
+      <Button theme='solid' className="bg-orange-400">User</Button>
     </Dropdown>
   ) : (
     <Dropdown
@@ -58,24 +53,43 @@ const Navbar = () => {
         </Dropdown.Menu>
       }
     >
-      <Button>SignUp/SignIn</Button>
+      <Button theme='solid' className="bg-orange-400">SignUp/SignIn</Button>
     </Dropdown>
   );
 
   return (
-    <div style={{ width: '100%' }}>
+    <div className="w-full">
       <Nav
+        className="bg-gradient-to-r from-cyan-500 to-blue-500"
         mode={'horizontal'}
         items={
           auth
             ? [
-                { itemKey: 'home', text: 'Home', icon: <IconIntro /> },
-                { itemKey: 'events', text: 'Events', icon: <IconCalendar /> },
-                { itemKey: 'profile', text: 'Profile', icon: <IconDescriptions /> },
+                {
+                  itemKey: '/',
+                  text: 'Home',
+                  icon:
+                    location.pathname === '/' ? (
+                      <IconHome className="text-blue-600" />
+                    ) : (
+                      <IconIntro />
+                    ),
+                },
+                {
+                  itemKey: '/events',
+                  text: 'Events',
+                  icon:
+                    location.pathname === '/events' ? <IconCalendar /> : <IconCalendarStroked />,
+                },
+                {
+                  itemKey: '/profile',
+                  text: 'Profile',
+                  icon: location.pathname === '/profile' ? <IconAvatar /> : <IconUserStroked />,
+                },
               ]
             : []
         }
-        onSelect={(key) => console.log(key)}
+        onSelect={(item) => navigate(item.itemKey as string)}
         header={{
           text: 'EMS',
         }}
