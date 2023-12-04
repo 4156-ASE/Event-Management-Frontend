1ef9743084f5e80e52c5d6f7c050651f1d90b582
Xzh0u <xzh0u.sxz@gmail.com>
2023-12-04 14:51:54 -0500

    feat(events): events table link to detail and edit page

--- src/components/event/EventList.tsx
+++ src/components/event/EventList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Avatar, Button, ButtonGroup, Table } from '@douyinfe/semi-ui';
 import * as dateFns from 'date-fns';
 import { APIs } from '../../utils/api';
@@ -8,70 +8,6 @@ import { Toast } from '@douyinfe/semi-ui';
 import { EventDetail } from '../../utils/dto';
 import { ColumnProps } from '@douyinfe/semi-ui/lib/es/table/interface';
 import { getName } from '../../utils/data';
-
-const columns: ColumnProps<EventDetail>[] = [
-  {
-    title: 'Title',
-    dataIndex: 'title',
-    width: 300,
-    sorter: (a, b) => a!.title.localeCompare(b!.title),
-    render: (text: string) => {
-      return <div>{text}</div>;
-    },
-  },
-  {
-    title: 'Location',
-    dataIndex: 'location',
-    sorter: (a, b) => a!.location.localeCompare(b!.location),
-    render: (text: string) => text,
-  },
-  {
-    title: 'Host',
-    dataIndex: 'host',
-    sorter: (a, b) => getName(a!.host).localeCompare(getName(b!.host)),
-    render: (host: EventDetail['host']) => {
-      const name = getName(host);
-      return (
-        <div>
-          <Avatar size="small" color={'orange'} style={{ marginRight: 4 }}>
-            {typeof name === 'string' && name.slice(0, 1)}
-          </Avatar>
-          {name}
-        </div>
-      );
-    },
-  },
-  {
-    title: 'Start Time',
-    dataIndex: 'start_time',
-    sorter: (a, b) => a!.start_time.localeCompare(b!.start_time),
-    render: (value) => {
-      return dateFns.format(new Date(value), 'dd/MM/yyyy-hh:mm:ss');
-    },
-  },
-  {
-    title: 'End Time',
-    dataIndex: 'end_time',
-    sorter: (a, b) => a!.end_time.localeCompare(b!.end_time),
-    render: (value) => {
-      return dateFns.format(new Date(value), 'dd/MM/yyyy-hh:mm:ss');
-    },
-  },
-  {
-    title: 'Operations',
-    fixed: 'right',
-    width: 220,
-    align: 'center',
-    dataIndex: 'operate',
-    render: () => (
-      <ButtonGroup type={'primary'}>
-        <Button>Detail</Button>
-        <Button>Edit</Button>
-        <Button type="danger">Delete</Button>
-      </ButtonGroup>
-    ),
-  },
-];
 
 const EventList = () => {
   const [dataSource, setData] = useState<EventDetail[]>([]);
@@ -91,8 +27,6 @@ const EventList = () => {
     })();
   }, []);
 
-  const navigate = useNavigate();
-
   const handleDelete = async (eventId: number | null) => {
     console.log('delete:', eventId);
     axios
@@ -105,6 +39,76 @@ const EventList = () => {
         console.log(err);
       });
   };
+
+  const columns: ColumnProps<EventDetail>[] = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      width: 300,
+      sorter: (a, b) => a!.title.localeCompare(b!.title),
+      render: (text: string) => {
+        return <div>{text}</div>;
+      },
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      sorter: (a, b) => a!.location.localeCompare(b!.location),
+      render: (text: string) => text,
+    },
+    {
+      title: 'Host',
+      dataIndex: 'host',
+      sorter: (a, b) => getName(a!.host).localeCompare(getName(b!.host)),
+      render: (host: EventDetail['host']) => {
+        const name = getName(host);
+        return (
+          <div>
+            <Avatar size="small" color={'orange'} style={{ marginRight: 4 }}>
+              {typeof name === 'string' && name.slice(0, 1)}
+            </Avatar>
+            {name}
+          </div>
+        );
+      },
+    },
+    {
+      title: 'Start Time',
+      dataIndex: 'start_time',
+      sorter: (a, b) => a!.start_time.localeCompare(b!.start_time),
+      render: (value) => {
+        return dateFns.format(new Date(value), 'dd/MM/yyyy-hh:mm:ss');
+      },
+    },
+    {
+      title: 'End Time',
+      dataIndex: 'end_time',
+      sorter: (a, b) => a!.end_time.localeCompare(b!.end_time),
+      render: (value) => {
+        return dateFns.format(new Date(value), 'dd/MM/yyyy-hh:mm:ss');
+      },
+    },
+    {
+      title: 'Operations',
+      fixed: 'right',
+      width: 220,
+      align: 'center',
+      dataIndex: 'operate',
+      render: (_, event: EventDetail) => {
+        return (
+          <ButtonGroup type={'primary'}>
+            <Link to={`/event/details/${event.id}`}>
+              <Button>Detail</Button>
+            </Link>
+            <Link to={`/event/${event.id}`}>
+              <Button>Edit</Button>
+            </Link>
+            <Button type="danger">Delete</Button>
+          </ButtonGroup>
+        );
+      },
+    },
+  ];
 
   return (
     <Table<EventDetail>
